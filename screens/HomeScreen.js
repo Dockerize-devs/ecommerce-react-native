3aa9e7d194d3290358d1b7b9024e1a34dd8e2bb7
lenin <justtlenin@gmail.com>
2020-06-16 19:03:33 +0600

    added title and filter button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
+import { Title, Button } from 'react-native-paper';
 import ProductCardHalf from '../components/product/ProductCardHalf';
+import Colors from '../data/constants/Colors';
 
 
 export default ({ navigation }) => {
     return (
         <ScrollView>
             <View style={styles.productsContainer}>
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
-                <ProductCardHalf />
+                <View style={styles.header}>
+                    <Title style={styles.title}>Winter Furniture Sale</Title>
+                    <Button style={styles.button} color={Colors.primary} mode='outlined'>Filter</Button>
+                </View>
+                <View style={styles.products}>
+                    <ProductCardHalf />
+                    <ProductCardHalf />
+                    <ProductCardHalf />
+                    <ProductCardHalf />
+                    <ProductCardHalf />
+                    <ProductCardHalf />
+                    <ProductCardHalf />
+                </View>
             </View>
         </ScrollView>
     );
@@ -25,8 +29,26 @@ export default ({ navigation }) => {
 
 const styles = StyleSheet.create({
     productsContainer: {
+        flex: 1
+    },
+    header: {
+        flex: 1,
+        marginHorizontal: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 15
+    },
+    products: {
         flex: 1,
         flexWrap: 'wrap',
         flexDirection: 'row',
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+    button: {
+        borderWidth: 1,
+        borderColor: Colors.primary
     }
 })
