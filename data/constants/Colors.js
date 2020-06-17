e8bc55125d270ce832066cb04c1640275f8af3f4
lenin <justtlenin@gmail.com>
2020-06-17 16:24:03 +0600

    added Accordion for filters

--- data/constants/Colors.js
+++ data/constants/Colors.js
@@ -1,6 +1,7 @@
 export default {
     primary: '#0C85D8',
     lightGray: '#d1d1d1',
+    veryLightGray: '#fafafa',
     gray: '#868686',
     darkWhite: '#dfdfdf'
 }
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Button, Text, Title, Divider } from 'react-native-paper';
+import { Button, Text, Title, Divider, List } from 'react-native-paper';
 import Colors from '../data/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import AppTitle from '../components/typography/AppTitle';
 
 
 const FilterItem = props => {
-    const { isActive } = props;
+    const { pisActive } = props;
+    const [isActive, setIsActive] = useState(pisActive)
     return (
         <Button
+            onPress={() => setIsActive(!isActive)}
             color={isActive ? 'white' : Colors.primary}
-            style={{ ...styles.filterItem, ...{ backgroundColor: isActive ? Colors.primary : '' } }}
+            style={{ ...styles.filterItem, ...{ backgroundColor: isActive ? Colors.primary : 'white' } }}
             mode='outlined'
             uppercase={false}
             labelStyle={{ fontSize: 12, padding: -100 }}
@@ -22,6 +24,9 @@ const FilterItem = props => {
 }
 
 export default props => {
+    const [isOpenPrice, setIsOpenPrice] = useState(false);
+    const [isOpenReview, setIsOpenReview] = useState(false);
+
     return (
         <View style={styles.screen}>
             <View style={styles.headerContainer}>
@@ -41,18 +46,58 @@ export default props => {
                     </View>
                 </View>
                 <View style={styles.filter}>
-                    <Title style={styles.filterTitle}>SORT BY</Title>
+                    <List.Accordion
+                        title={<Title style={styles.filterTitle}>PRICE</Title>}
+                        expanded={isOpenReview}
+                        onPress={() => setIsOpenReview(!isOpenReview)}
+                    >
+                        <View style={styles.filterItems}>
+                            <FilterItem>$$ <Text style={styles.productCount}>(14)</Text></FilterItem>
+                            <FilterItem isActive={true}>$$$ <Text style={styles.productCount}>(7)</Text></FilterItem>
+                            <FilterItem>$$$$$ <Text style={styles.productCount}>(11)</Text></FilterItem>
+                        </View>
+                    </List.Accordion>
                     <Divider />
-                    <View style={styles.filterItems}>
-                        <FilterItem isActive={true}>Most Popular</FilterItem>
-                        <FilterItem>Newly Added</FilterItem>
-                        <FilterItem>Lowest Price</FilterItem>
-                        <FilterItem>Highest Price</FilterItem>
-                        <FilterItem>Biggest Saving</FilterItem>
-                    </View>
+                </View>
+                <View style={styles.filter}>
+                    <List.Accordion
+                        title={<Title style={styles.filterTitle}>SHOP</Title>}
+                        expanded={isOpenReview}
+                        onPress={() => setIsOpenReview(!isOpenReview)}
+                    >
+                        <View style={styles.filterItems}>
+                            <FilterItem>$$ <Text style={styles.productCount}>(14)</Text></FilterItem>
+                            <FilterItem isActive={true}>$$$ <Text style={styles.productCount}>(7)</Text></FilterItem>
+                            <FilterItem>$$$$$ <Text style={styles.productCount}>(11)</Text></FilterItem>
+                        </View>
+                    </List.Accordion>
+                    <Divider />
+                </View>
+                <View style={styles.filter}>
+                    <List.Accordion
+                        title={<Title style={styles.filterTitle}>REVIEW</Title>}
+                        expanded={isOpenPrice}
+                        onPress={() => setIsOpenPrice(!isOpenPrice)}
+                    >
+                        <View style={styles.filterItems}>
+                            <FilterItem>ANY <Text style={styles.productCount}>(14)</Text></FilterItem>
+                            <FilterItem> ★★★★★ <Text style={styles.productCount}>(14)</Text></FilterItem>
+                            <FilterItem isActive={true}>★★★★ <Text style={styles.productCount}>(7)</Text></FilterItem>
+                            <FilterItem>★★★ <Text style={styles.productCount}>(11)</Text></FilterItem>
+                            <FilterItem>★★ <Text style={styles.productCount}>(11)</Text></FilterItem>
+                            <FilterItem>★ <Text style={styles.productCount}>(11)</Text></FilterItem>
+                        </View>
+                    </List.Accordion>
                 </View>
             </View>
-
+            <View style={styles.actionArea}>
+                <Button
+                    onPress={props.navigation.goBack}
+                    style={styles.applyFilterBtn}
+                    mode='outlined'
+                    color={Colors.primary}>
+                    Apply Filters</Button>
+            </View>
         </View>
     );
 }
@@ -67,8 +112,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        borderBottomWidth: 2,
-        borderBottomColor: Colors.lightGray
+        borderBottomWidth: 1,
+        borderBottomColor: Colors.gray
     },
     headerText: {
         marginLeft: 10,
@@ -80,14 +125,31 @@ const styles = StyleSheet.create({
         fontSize: 15
     },
     filterItems: {
-        marginVertical: 5,
+        marginBottom: 5,
         flexDirection: 'row',
         flexWrap: 'wrap',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundColor: Colors.veryLightGray,
+        paddingVertical: 10,
+        borderRadius: 10
     },
     filterItem: {
         borderRadius: 20,
         fontSize: 10,
         margin: 5,
+    },
+    productCount: {
+        fontSize: 8
+    },
+    actionArea: {
+        flex: 1,
+        flexDirection: 'column-reverse'
+    },
+    applyFilterBtn: {
+        borderColor: Colors.primary,
+        borderWidth: 1,
+        paddingVertical: 5,
+        marginVertical: 20,
+        marginHorizontal: 10
     }
 })
